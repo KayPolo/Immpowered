a12341e9256d319caaf925fb1fa2994ade5b7b5c
Karla Polo-Garcia <polok534@newschool.edu>
2017-10-12 22:22:28 -0400

    Second commit

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
  window.addEventListener('DOMContentLoaded', function(e){
  	e.preventDefault();
- 	attachEvents();
+ 	// attachEvents();
 
 	// var navOffset = $('nav').offset().top;
 
@@ -12,32 +12,32 @@
 		//});
 
 
-		$(window).scroll(function(){
-			var scrollPos = $(window).scrollTop();
+		// $(window).scroll(function(){
+		// 	var scrollPos = $(window).scrollTop();
 
-			console.log(scrollPos);
-
-
+		// 	console.log(scrollPos);
 
 
- });
+
+
+ // });
 });
 
 
-  	function attachEvents(){
+  	// function attachEvents(){
 
-  		$('.sideNav a').click(function(e) {
-			e.preventDefault();
+  	// 	$('.sideNav a').click(function(e) {
+			// e.preventDefault();
 
-	     	var myTarget = $(this.hash);
-	     	myTarget = myTarget.length && myTarget
+	  //    	var myTarget = $(this.hash);
+	  //    	myTarget = myTarget.length && myTarget
 	      			
-	        var targetOffset = myTarget.offset().top;
-	       	$('html,body').animate({scrollTop: targetOffset}, 1000);
+	  //       var targetOffset = myTarget.offset().top;
+	  //      	$('html,body').animate({scrollTop: targetOffset}, 1000);
 	
-	     });
+	  //    });
 
-  	}
+  	// }
 
 
 
@@ -46,7 +46,7 @@
 
 
 function openNav(){
-	mySideNav.style.width="30vw";
+	mySideNav.style.width="100vw";
 	
 	
 
@@ -58,47 +58,23 @@ function closeNav(){
 
 }
 
-document.addEventListener('DOMContentLoaded', function(){ //since we don't have anything to triger the action of pushing all the links
-														  //inside the HTML, we need to use this statement
-														  //the meaning of this statement is when the you load the DOM, load as well the 
-														  //function that contains all the information inside...
+document.addEventListener('DOMContentLoaded', function(){ 
+function addLinks(parent){ 
 
-//There are two ways to do this. This is the longer, but easier to understand
-
-// 	var topNav = document.getElementById("myTopNav"); 
-// 	topNav.innerHTML += "<a href = 'index.html'>HOME</a>"; 
-// 	topNav.innerHTML += "<a href = 'about.html'>ABOUT</a>";
-// 	topNav.innerHTML += "<a href = 'work.html'>WORK</a>";
-// 	topNav.innerHTML += "<a href = 'more.html'>MORE</a>"; 
-// 	topNav.innerHTML += "<a href = 'contact.html'>CONTACT</a>";
-
-// 	var sideNav = document.getElementById("mySideNav");
-// 	sideNav.innerHTML += "<a href = 'index.html'>HOME</a>"; 
-// 	sideNav.innerHTML += "<a href = 'about.html'>ABOUT</a>";
-// 	sideNav.innerHTML += "<a href = 'work.html'>WORK</a>";
-// 	sideNav.innerHTML += "<a href = 'more.html'>MORE</a>"; 
-// 	sideNav.innerHTML += "<a href = 'contact.html'>CONTACT</a>";   
-
-// });
-
-
-//This is and optimized version 
-function addLinks(parent){ //here I'm using an argument for my function
-							// so I can astore all the information without repeating it
-	parent.innerHTML += "<a href = '#home'>HOME</a>"; 
-	parent.innerHTML += "<a href = '#grid-one'>GRID OF 9</a>";
-	parent.innerHTML += "<a href = '#rq'>RESEARCH QUESTIONS</a>";
-	parent.innerHTML += "<a href = '#inspiration'>INSPIRATION GRID</a>";
-	parent.innerHTML += "<a href = '#aesthetic'>AESTHETIC GRID</a>";
-	parent.innerHTML += "<a href = '#refined'>REFINED R.Q.</a>";
+	parent.innerHTML += "<a href = '#about'>About</a>"; 
+	parent.innerHTML += "<a href = '#favorites'>Favorites</a>";
+	parent.innerHTML += "<a href = '#faq'>FAQ</a>";
+	parent.innerHTML += "<a href = '#contact'>Contact</a>";
 }
 
 	
 
 	var sideNav = document.getElementById("mySideNav");
-	addLinks(sideNav);  // here I'm saying, take all the information inside the
-						// function addLink and substitute the argument (in this case the 
-						//argument name is parent) with the variable sideNav 
+	addLinks(sideNav);  
+
+	var topNav = document.getElementById("myTopNav");
+	addLinks(topNav);
+
 });
 
 
